86760ca1c870f15b3e72a506eb2e051e4a23dd46
zohravali <valiyevazohra@gmail.com>
2022-01-22 19:32:21 +0400

    added Event

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Event;
+using System;
+using System.Text;
+using static Event.Run;
+
+namespace ConsoleApp11
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+
+            var str = Console.ReadLine();
+            MyClass cls = new MyClass();
+            Func funcDell = cls.Space;
+            funcDell += cls.Reverse;
+
+            Run run = new Run();
+            run.RunFunc(funcDell, str);
+        }
+    }
+}
---
+++ Run.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    public delegate void Func(string word);
+    class MyClass
+    {
+        public void Space(string word)
+        {
+            StringBuilder word1 = new();
+            word1.Append(word[0]);
+            for (int i = 1; i < word.Length; i++)
+            {
+                word1.Append('_');
+                word1.Append(word[i]);
+            }
+            Console.WriteLine(word1);
+        }
+
+        public void Reverse(string word)
+        {
+            StringBuilder word1 = new();
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                word1.Append(word[i]);
+            }
+
+            Console.WriteLine(word1);
+        }
+    }
+
+    class Run
+    {
+
+        public void RunFunc(Func func, string word)
+        {
+            func.Invoke(word);
+        }
+    }
+}
